f833ae6d863520108518fb170dbed285ff1e96f2
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-08-15 18:40:33 +0530

    wrap-up

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -3,12 +3,7 @@ import AppReducer from "./AppReducer";
 
 // Initial State
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [],
 };
 
 // Create context
@@ -45,3 +40,10 @@ export const GlobalProvider = ({ children }) => {
     </GlobalContext.Provider>
   );
 };
+
+// sample data
+
+// { id: 1, text: "Flower", amount: -20 },
+// { id: 2, text: "Salary", amount: 300 },
+// { id: 3, text: "Book", amount: -10 },
+// { id: 4, text: "Camera", amount: 150 },
